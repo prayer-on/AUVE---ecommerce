2f2d65b6790107a4d4662d2b4ba8e564f579304e
prayer-on <prayer.onaiwu@gmail.com>
2026-09-05 00:13:04 +0200

    fix: explicit contactus import string change

--- Frontend/src/Pages/UserContact.tsx
+++ Frontend/src/Pages/UserContact.tsx
@@ -1,4 +1,4 @@
-import Contactus from "../Components/Contactus";
+import Contactus from "../Components/ContactUs";
 import Banner from "../Components/Banner";
 import Sidebar from "../Components/Sidebar";
 import Footer from "../Components/Footer";
